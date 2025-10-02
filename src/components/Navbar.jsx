a38f8a60bbb0562ebaa484546a653106e54224bd
Eluwole-David <davidtimileyin55@gmail.com>
2025-10-02 18:05:40 +0100

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const FadeInUp = {
   initial: { opacity: 0, y: 30 },
@@ -13,7 +14,15 @@ const staggerContainer = {
     },
   },
 };
+
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openNav = () => {
+    setIsOpen(true);
+  };
+  const closeNav = () => {
+    setIsOpen(false);
+  };
   return (
     <motion.nav
       className="navbar"
@@ -29,8 +38,26 @@ export const Navbar = () => {
       >
         DaveTech
       </motion.div>
+      <motion.div
+        className="menu"
+        variants={FadeInUp}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        onClick={openNav}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="menu-icon"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#f59e0b"
+        >
+          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+        </svg>
+      </motion.div>
       <motion.ul
-        className="Navlinks"
+        className={`Navlinks ${isOpen ? "open" : ""}`}
         variants={staggerContainer}
         initial="initial"
         animate="animate"
@@ -39,6 +66,28 @@ export const Navbar = () => {
           variants={FadeInUp}
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
+          onClick={closeNav}
+          className="Xmenu"
+        >
+          {" "}
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="Xmenu-icon"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#f59e0b"
+            >
+              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
+          </a>
+        </motion.li>
+        <motion.li
+          variants={FadeInUp}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          onClick={closeNav}
         >
           {" "}
           <a href="#home">Home</a>
@@ -47,6 +96,7 @@ export const Navbar = () => {
           variants={FadeInUp}
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
+          onClick={closeNav}
         >
           <a href="#projects">Project</a>
         </motion.li>
@@ -54,6 +104,7 @@ export const Navbar = () => {
           variants={FadeInUp}
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
+          onClick={closeNav}
         >
           <a href="#contact">Contact</a>
         </motion.li>
